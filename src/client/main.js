d6af28c86cd44fbfdcfa73858a10d60d962285a8
yaegerbomb42 <yaeger.james42@gmail.com>
2025-06-23 01:17:49 -0500

    feat: display connection status

--- src/client/main.js
+++ src/client/main.js
@@ -9,6 +9,7 @@ class Game {
     this.engine = null;
     this.socket = null;
     this.loadingManager = new LoadingManager();
+    this.isConnected = false;
     this.isInitialized = false;
   }
 
@@ -31,11 +32,14 @@ class Game {
       // Start game loop
       this.startGameLoop();
       this.loadingManager.updateProgress(100, 'Ready!');
-      
+
       // Hide loading screen
       setTimeout(() => {
         this.loadingManager.hide();
         this.showGameUI();
+        if (this.engine && this.engine.uiManager) {
+          this.engine.uiManager.updateConnectionStatus(this.isConnected);
+        }
       }, 500);
       
       this.isInitialized = true;
@@ -49,15 +53,30 @@ class Game {
   async initSocket() {
     return new Promise((resolve, reject) => {
       this.socket = io('http://localhost:3001');
-      
+
       this.socket.on('connect', () => {
         console.log('Connected to game server');
+        this.isConnected = true;
+        if (this.engine && this.engine.uiManager) {
+          this.engine.uiManager.updateConnectionStatus(true);
+        }
         resolve();
       });
-      
+
       this.socket.on('connect_error', (error) => {
         console.error('Connection failed:', error);
+        this.isConnected = false;
+        if (this.engine && this.engine.uiManager) {
+          this.engine.uiManager.updateConnectionStatus(false);
+        }
         reject(error);
+      });
+
+      this.socket.on('disconnect', () => {
+        this.isConnected = false;
+        if (this.engine && this.engine.uiManager) {
+          this.engine.uiManager.updateConnectionStatus(false);
+        }
       });
       
       this.socket.on('playerUpdate', (data) => {
@@ -97,6 +116,7 @@ class Game {
 // Initialize game when page loads
 window.addEventListener('load', () => {
   const game = new Game();
+  window.game = game;
   game.init();
 });
 
